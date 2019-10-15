e80a273d156647aa7329e06f103a6372e19b05da
ThanhPhu <thanhphu183@gmail.com>
2019-10-15 16:39:28 +0700

    add-db-seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(seed_setting_table::class);
+        $this->call(seed_user_table::class);
     }
 }
--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -36,8 +36,8 @@
             </button>
             <div class="dropdown-menu">
                 <a href="" class="dropdown-item">Thay đổi thông tin cá nhân</a>
-                <a href="" class="dropdown-item">Đổi mật khẩu</a>
-                <a href="signin.html" class="dropdown-item">Thoát</a>
+                <a href="{{ route('password.change') }}" class="dropdown-item">Đổi mật khẩu</a>
+                <a href="signin.html" class="btn btn-sm dropdown-item" id="power-button"  onclick="event.preventDefault();document.getElementById('logout').submit();">Thoát</a>
             </div>
             <a href="signin.html" class="btn btn-sm" id="power-button"  onclick="event.preventDefault();document.getElementById('logout').submit();">
                 <i class="material-icons">

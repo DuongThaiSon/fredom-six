609e747add5e29ca800f22e9950e569645ea534d
l3lackheart <dgbao1340@gmail.com>
2019-10-24 11:33:43 +0700

    fix: truncate assets link

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -12,7 +12,7 @@
      <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
      <!-- CSS Files -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/lux/bootstrap.min.css" integrity="sha256-2AE13SXoJY6p0WSPAlYEZpalYyQ1NiipAwSt3s60n8M=" crossorigin="anonymous"/>
-     <link rel="stylesheet" href="{{ asset('assets/admin') }}/css/style.css" />
+     <link rel="stylesheet" href="/assets/admin/css/style.css" />
      @stack('css')
 </head>
 <body>
@@ -45,9 +45,9 @@
     <!-- Jquery -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
     <script src="https://cdn.ckeditor.com/4.12.1/full-all/ckeditor.js"></script>
-    <script src="{{ asset('assets/admin') }}/js/jquery-3.4.1.min.js"></script>
-    <script src="{{ asset('assets/admin') }}/js/jquery-ui.js"></script>
-    <script type="text/javascript" src="{{ asset('assets/admin') }}/js/main.js"></script>
+    <script src="/assets/admin/js/jquery-3.4.1.min.js"></script>
+    <script src="/assets/admin/js/jquery-ui.js"></script>
+    <script type="text/javascript" src="/assets/admin/js/main.js"></script>
     @stack('js')
 </body>
 </html>

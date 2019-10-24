716a384deb3d659abd84c4bc2ab61bb7f182c170
l3lackheart <dgbao1340@gmail.com>
2019-10-24 18:07:54 +0700

    feature: auto active sidebar

--- public/assets/admin/js/main.js
+++ public/assets/admin/js/main.js
@@ -1,94 +1,142 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/admin/main.js":
+/*!************************************!*\
+  !*** ./resources/js/admin/main.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
 $.ajaxSetup({
   headers: {
-      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
   }
 });
-// Toggle Icon
-$(document).ready(function() {
- $(".toggle-icon").click(function() {
-  if ($(this).hasClass("text-primary")) {
-   $(this)
-    // .text("highlight_off")
-    .removeClass("text-primary");
-  } else {
-   $(this)
-    // .text("check_circle_outline")
-    .addClass("text-primary");
-  }
- });
- // Sort table
- $(function() {
-  $(".sort").sortable();
-  $(".sort").disableSelection();
- });
+$(document).ready(function () {
+  var pageId = $("#page-id").val(); // Add active class
 
- // Sidebar toggle
- $(document).ready(function() {
-  $("#sidebarCollapse").on("click", function() {
-   $("#sidebar").toggleClass("active");
-   $("#page-content").toggleClass("active");
-   $(".has-child").hover(function() {
-    $("#page-content").toggleClass("index");
-   });
-   $(this).toggleClass("active");
-   if ($(this).hasClass("active")) {
-    $(".collapse").removeClass("show");
-   }
-   $(".sub").toggleClass("collapse");
-   $(".nav-link.collapsed").attr("data-toggle", function(i, attr) {
-    return attr == "collapse" ? "false" : "collapse";
-   });
+  $(".menu-" + pageId).parent().addClass("active");
+  $(".menu-" + pageId).parents("div").addClass("show");
+  $(".menu-" + pageId).parent("li").parents("li").addClass("active");
+  console.log(pageId); // sidebar toggle
+
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+    $("#page-content").toggleClass("active");
+    $(".has-child").hover(function () {
+      $("#page-content").toggleClass("index");
+    });
+    $(this).toggleClass("active");
+
+    if ($(this).hasClass("active")) {
+      $(".collapse").removeClass("show");
+    }
+
+    $(".sub").toggleClass("collapse");
+    $(".nav-link.collapsed").attr("data-toggle", function (i, attr) {
+      return attr == "collapse" ? "false" : "collapse";
+    });
   });
- });
-
- //  Sortable
- $(".sort").sortable({
-  handle: ".connect"
- });
-
- // Login input
- $(".toggle-password").click(function() {
-  var input = $($(this).attr("toggle"));
-  if (input.attr("type") == "password") {
-   input.attr("type", "text");
-  } else {
-   input.attr("type", "password");
-  }
- });
- $(".delete-email").click(function() {
-  $("#email-field").val("");
- });
-
- //Check all button
-
- $("#btn-ck-all").click(function() {
-  var checkBoxes = $(".checkdel");
-  checkBoxes.prop("checked", !checkBoxes.prop("checked"));
-  if (
-   $("input.checkdel:checkbox:checked").length ==
-   $("input.checkdel:checkbox").length
-  ) {
-   $("#btn-ck-all i").text("check_box");
-  } else if ($("input.checkdel:checkbox:checked").length > 0) {
-   $("#btn-ck-all i").text("indeterminate_check_box");
-  } else {
-   $("#btn-ck-all i").text("check_box_outline_blank");
-  }
- });
-
- $(".checkdel").change(function() {
-  if (
-   $("input.checkdel:checkbox:checked").length ==
-   $("input.checkdel:checkbox").length
-  ) {
-   $("#btn-ck-all i").text("check_box");
-  } else if ($("input.checkdel:checkbox:checked").length > 0) {
-   $("#btn-ck-all i").text("indeterminate_check_box");
-  } else {
-   $("#btn-ck-all i").text("check_box_outline_blank");
-  }
- });
-
- // Date Picker
- $(".datepicker").datepicker({ showButtonPanel: true, dateFormat: "yy-mm-dd" });
 });
+
+/***/ }),
+
+/***/ 0:
+/*!******************************************!*\
+  !*** multi ./resources/js/admin/main.js ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /mnt/d/Projects/CMS/Leotive-CMS-v3/resources/js/admin/main.js */"./resources/js/admin/main.js");
+
+
+/***/ })
+
+/******/ });
---
+++ resources/js/admin/main.js
@@ -0,0 +1,39 @@
+$.ajaxSetup({
+    headers: {
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+    }
+});
+
+$(document).ready(function() {
+    var pageId = $("#page-id").val();
+    // Add active class
+    $(".menu-" + pageId)
+        .parent()
+        .addClass("active");
+    $(".menu-" + pageId)
+        .parents("div")
+        .addClass("show");
+    $(".menu-" + pageId)
+        .parent("li")
+        .parents("li")
+        .addClass("active");
+
+    console.log(pageId);
+
+    // sidebar toggle
+    $("#sidebarCollapse").on("click", function() {
+        $("#sidebar").toggleClass("active");
+        $("#page-content").toggleClass("active");
+        $(".has-child").hover(function() {
+            $("#page-content").toggleClass("index");
+        });
+        $(this).toggleClass("active");
+        if ($(this).hasClass("active")) {
+            $(".collapse").removeClass("show");
+        }
+        $(".sub").toggleClass("collapse");
+        $(".nav-link.collapsed").attr("data-toggle", function(i, attr) {
+            return attr == "collapse" ? "false" : "collapse";
+        });
+    });
+});

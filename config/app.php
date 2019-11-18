eb12cc5da30c943861486ffaa72532600837a4f5
ThanhPhu <thanhphu183@gmail.com>
2019-11-18 11:29:17 +0700

    fix provider #1

--- config/app.php
+++ config/app.php
@@ -172,7 +172,6 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        App\Providers\PartialServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 

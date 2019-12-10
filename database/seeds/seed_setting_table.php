77d1846416899b07dae482ea38761071b359b7a1
l3lackheart <dgbao1340@gmail.com>
2019-12-10 23:34:35 +0700

    feat: update settings seeder

--- database/seeds/seed_setting_table.php
+++ database/seeds/seed_setting_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class seed_setting_table extends Seeder
 {
@@ -11,31 +12,40 @@ class seed_setting_table extends Seeder
      */
     public function run()
     {
-        DB::table('settings')->insert(
-            array(
-                'company_name' => 'LEOTIVE',
-                'company_address'=> 'Company address',
-                'company_website_url'=> 'http://www.leotive.com',
-                'company_tel'=> '1900292976',
-                'company_hotline'=> '1900292976',
-                'company_mobile'=> '1900292976',
-                'company_email'=> 'hi@leotive.com',
-                'company_facebook_url'=> 'http://www.facebook.com/leotivepage/',
-                'email_smtp_server'=> 'smtp.gmail.com',
-                'email_smtp_port'=> '578',
-                'email_smtp_user'=> 'no-reply@leotive.com',
-                'email_smtp_pass'=> 'leotive123',
-                'email_smtp_name'=> 'LEOTIVE - Automatic email',
-                'email_smtp_email_address'=> 'no-reply@leotive.com',
-                'seo_page_title'=> 'LEOTIVE',
-                'seo_meta_des'=> 'LEOTIVE',
-                'seo_meta_keywords'=> 'LEOTIVE',
-                'seo_meta_copyright'=> 'LEOTIVE',
-                'seo_meta_author'=> 'LEOTIVE',
-                'seo_meta_page_topic'=> 'LEOTIVE',
-                'company_instagram_url'=> 'https://www.instagram.com/',
-                'company_youtube_url'=> 'https://www.youtube.com/',
-            )
+        $data = array(
+            'company_name' => 'LEOTIVE',
+            'company_address'=> 'Company address',
+            'company_website_url'=> 'http://www.leotive.com',
+            'company_tel'=> '1900292976',
+            'company_hotline'=> '1900292976',
+            'company_mobile'=> '1900292976',
+            'company_email'=> 'hi@leotive.com',
+            'company_facebook_url'=> 'http://www.facebook.com/leotivepage/',
+            'email_smtp_server'=> 'smtp.gmail.com',
+            'email_smtp_port'=> '578',
+            'email_smtp_user'=> 'no-reply@leotive.com',
+            'email_smtp_pass'=> 'leotive123',
+            'email_smtp_name'=> 'LEOTIVE - Automatic email',
+            'email_smtp_email_address'=> 'no-reply@leotive.com',
+            'seo_page_title'=> 'LEOTIVE',
+            'seo_meta_des'=> 'LEOTIVE',
+            'seo_meta_keywords'=> 'LEOTIVE',
+            'seo_meta_copyright'=> 'LEOTIVE',
+            'seo_meta_author'=> 'LEOTIVE',
+            'seo_meta_page_topic'=> 'LEOTIVE',
+            'company_instagram_url'=> 'https://www.instagram.com/',
+            'company_youtube_url'=> 'https://www.youtube.com/',
         );
+
+        $order = count($data);
+        foreach ($data as $key => $value) {
+            DB::table('settings')->insertGetId([
+                'key' => $key,
+                'display_name' => Str::title(str_replace("_", " ", $key)),
+                'value' => $value,
+                'order' => $order--
+            ]);
+        }
+
     }
 }

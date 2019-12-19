c978338e4c9e965e568c9270b47f6d107df61f3d
l3lackheart <dgbao1340@gmail.com>
2019-12-20 00:38:38 +0700

    feat: cover forgot password layout

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,4 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
-    Route::resource('product', 'ProductController');
 });
